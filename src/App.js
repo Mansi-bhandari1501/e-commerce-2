85794d1ff1e59431d92592cb36b9bb467f332d45
Mansi-bhandari1501 <mansi.bhandari1501@gmail.com>
2024-01-23 18:46:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Header from "./components/header";
+import Section1 from "./components/frontpage/section1"
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Section1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/frontpage/section1.jsx
@@ -0,0 +1,18 @@
+import Reactfrom from "react";
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import "./section1.css";
+
+function Section1(){
+    return(<div></div>
+        // <Carousel showThumbs="false">
+        //         <div>
+        //             {/* <img src="https://s3-alpha-sig.figma.com/img/b26f/ea69/ccfd8aa5825862cdb9604a4fb4930464?Expires=1707091200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=f5qH-PWH06u3TdhnWVqAvUAk4oIJ~nD0PSgUT4AzZT5Uj-u49FiDgxS6MZlSBipj4BaCZ~4Z8SM5at-h3CnfhxXm8ObPYnAQnodycMzyDcrJimkIEZ9tmG7LUQcWuB~CBAw-qX4y21j5yUSXpuqh39idWfrKY3HMAMMS9xjXczDAwt6be9WFg3kJ2IlrWoFQ2Oi5NX3b0t1fqTaBYjy~QsLe3OExUGSXfoufi9~G2FlbPQqHkeVeid4BgeY07UElbkMO5QeT4ZXRt3fzC7yNRHnCoXMbHOqPHRdf3jPk46AG8GrW78748BKm31vR2xdrWCB1rtnbtPfmdZiN~BzfsQ__" /> */}
+        //             {/* <p className="legend">Legend 1</p> */}
+        //             <img src={"src/assert/cover1.png"}/>
+        //         </div>
+        //     </Carousel>
+    )
+}
+export default Section1;
